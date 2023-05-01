1709b2dfab8c9f725a7977f7bb1cbe527c7ae028
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-01 12:53:29 +0300

    the get_youtube_data() function has been created

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import os
 from config import config
+import google.auth
+from googleapiclient.discovery import build
+
 
 from utils import get_youtube_data, create_database, save_data_to_database
 
 
 def main():
     api_key = os.getenv('YT-API-KEY')
+
     channel_ids = [
-        "@ColbertLateShow",
-        "@JimmyKimmelLive",
-        "@fallontonight",
-        "@LateNightSeth",
-        "@TheLateLateShow"
+        "UCMtFAi84ehTSYSE9XoHefig",  # "@ColbertLateShow",
+        # "UCa6vGFO9ty8v5KZJXQxdhaw",  # "@JimmyKimmelLive",
+        # "UC8-Th83bH_thdKZDJCrn88g",  # "@fallontonight",
+        # "UCVTyTA7-g9nopHeHbeuvpRA",  # "@LateNightSeth",
+        # "UCJ0uqCI0Vqr2Rrt1HseGirg",  # "@TheLateLateShow"
     ]
 
-    params = config()
     data = get_youtube_data(api_key, channel_ids)
-
-    create_database('YouTube_DB', params)
-    save_data_to_database(data, 'YouTube_DB', params)
-
+    params = config()
+    # create_database('YouTube_DB', params)
+    # save_data_to_database(data, 'YouTube_DB', params)
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -1,9 +1,29 @@
 from typing import Any
+from googleapiclient.discovery import build
 
 
 def get_youtube_data(api_key: str, channel_ids: list[str]) -> list[dict[str, Any]]:
     """Gets data about YouTube channels via API YouTube."""
-    pass
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    data = []
+    videos_data = []
+    next_page_token = None
+    for channel_id in channel_ids:
+        channel_data = youtube.channels().list(part='snippet,statistics', id=channel_id).execute()
+
+        while True:
+            response = youtube.search().list(part='id,snippet', channelId=channel_id, type='video',
+                                             order='date', maxResults=50, pageToken=next_page_token).execute()
+            videos_data.extend(response['items'])
+            next_page_token = response.get('nextPageToken')
+            if not next_page_token:
+                break
+        data.append({
+            'channel': channel_data['items'][0],
+            'videos': videos_data
+        })
+        return data
 
 def create_database(name: str, params: dict) -> None:
     """Creates database and tables"""
@@ -11,4 +31,3 @@ def create_database(name: str, params: dict) -> None:
 
 def save_data_to_database(data: list[dict[str, Any]], db_name: str, params: dict) -> None:
     """Saves data to the database"""
-
